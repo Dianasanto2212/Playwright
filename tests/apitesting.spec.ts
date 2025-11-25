c2b92c569557e3e70bcf64cd8a2e59648f2e2cd5
Dianasanto2212 <dianavargheese@gmail.com>
2025-11-25 19:16:23 +0530

    apitestingforall

--- tests/apitesting.spec.ts
+++ tests/apitesting.spec.ts
@@ -14,10 +14,43 @@ test('apitestingcheck',async()=>{
      expect (formattedresponse.data.email).toBe('george.bluth@reqres.in')
 }
 )
-test('apitestingcheck',async()=>{
+test('apitestingdelete',async()=>{
      let context= await request.newContext({
         baseURL:'https://reqres.in/',
         extraHTTPHeaders:{
             "x-api-key": "reqres-free-v1"
         }
      })
+     let response=await context.delete('api/users/1')
+     expect(response.status()).toBe(204) 
+    })
+    test('apitestingpost',async()=>{
+     let context= await request.newContext({
+        baseURL:'https://reqres.in/',
+        extraHTTPHeaders:{
+            "x-api-key": "reqres-free-v1"
+        }
+     })
+let response=await context.post("api/users",{
+data:{
+    "email": 'sureka@gmail.com',
+    "first_name": 'Trainer',
+}
+})
+expect(response.status()).toBe(201)
+    })
+    test('apitestingput',async()=>{
+     let context= await request.newContext({
+        baseURL:'https://reqres.in/',
+        extraHTTPHeaders:{
+            "x-api-key": "reqres-free-v1"
+        }
+     })
+let response=await context.put("api/users/1",{
+data:{
+    "email": 'sureka@gmail.com',
+    "first_name": 'Trainer',
+}
+})
+expect(response.status()).toBe(200)
+    })
